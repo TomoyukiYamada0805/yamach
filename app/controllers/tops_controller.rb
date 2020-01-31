295d414143eb72a8dedbf55b5abb90554309169c
Tomoyuki Yamada <b.vopen_59@i.softbank.jp>
2020-01-31 10:11:31 +0900

    横断検索追加

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,12 +1,12 @@
 class TopsController < ApplicationController
   def index
     @categories = Category.left_outer_joins(:category_name)
-    @q = ThreadList.ransack(params[:q])
+    @q = ThreadList.left_outer_joins(:category_name).left_outer_joins(:response).ransack(params[:q])
     @thread_lists = @q.result(distinct: true)
   end
 
   def search
-    @q = ThreadList.search(search_params)
+    @q = ThreadList.left_outer_joins(:category_name).left_outer_joins(:response).search(search_params)
     @thread_lists = @q.result(distinct: true)
   end
 
